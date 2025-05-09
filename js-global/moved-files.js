ca6297fd638798a2aca0c7aaed38ff28687ef106
Kumafuji1202 <131581988+Kumafuji1202@users.noreply.github.com>
2025-05-09 19:12:37 +0900

    move some tools to new "linguistics" directory

--- js-global/moved-files.js
+++ js-global/moved-files.js
@@ -1,4 +1,5 @@
 var movedFiles = [
+    // ディレクトリ全体を指定するときは最後に/を含めない
     {
         "name": {
             "en": "Rolling Sky Spritesheet Maker",
@@ -10,8 +11,35 @@ var movedFiles = [
     },
     {
         "name": {
+            "en": "X-SAMPA(-based) Phonetic Symbol Input Tool",
+            "en-Shaw": "⸰𐑦-𐑕𐑩𐑥𐑓𐑨(X-SAMPA)(-𐑚𐑱𐑕𐑑) 𐑓𐑩𐑯𐑧𐑑𐑦𐑒 𐑕𐑦𐑥𐑚𐑩𐑤 𐑦𐑯𐑐𐑫𐑑 𐑑𐑵𐑤",
+            "ja": "X-SAMPA 発音記号入力ツール",
+        },
+        "from": "/xsampa-input",
+        "to": "/linguistics/xsampa-ime"
+    },
+    {
+        "name": {
+            "en": "Shavian Soft Keyboard",
+            "en-Shaw": "·𐑖𐑱𐑝𐑾𐑯 𐑕𐑪𐑓𐑑 𐑒𐑰𐑚𐑹𐑛",
+            "ja": "ショー文字ソフトキーボード",
+        },
+        "from": "/shavian-keyboard",
+        "to": "/linguistics/shavian-keyboard"
+    },
+    {
+        "name": {
+            "en": "Arabic Script -> Arabic Presentation Forms-B Converter",
+            "en-Shaw": "·𐑨𐑮𐑩𐑚𐑦𐑒 𐑕𐑒𐑮𐑦𐑐𐑑 -> ·𐑨𐑮𐑩𐑚𐑦𐑒 𐑐𐑮𐑧𐑟𐑩𐑯𐑑𐑱𐑖𐑩𐑯 𐑓𐑹𐑥𐑟-B 𐑒𐑩𐑯𐑝𐑻𐑑𐑼",
+            "ja": "アラビア文字Unicodeブロック → アラビア文字表示形B 変換器",
+        },
+        "from": "/arabic-to-presentation",
+        "to": "/linguistics/arabic-to-presentation"
+    },
+    {
+        "name": {
             "en": "Foo",
-            "en-Shaw": "𐑓𐑵",
+            "en-Shaw": "·𐑓𐑵",
             "ja": "ほげ"
         },
         "from": "/foo",
