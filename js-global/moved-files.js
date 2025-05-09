9b3ce80c71a1c8294582a65e8abdf7c4864d99d3
Kumafuji1202 <131581988+Kumafuji1202@users.noreply.github.com>
2025-05-09 18:11:19 +0900

    automatic redirect system test

--- js-global/moved-files.js
+++ js-global/moved-files.js
@@ -1,9 +1,19 @@
 var movedFiles = [
     {
+        "name": {
+            "en": "Rolling Sky Spritesheet Maker",
+            "en-Shaw": "·𐑮𐑴𐑤𐑦𐑙 𐑕𐑒𐑲 𐑕𐑐𐑮𐑲𐑑𐑖𐑰𐑑 𐑡𐑧𐑯𐑼𐑱𐑑𐑼",
+            "ja": "ローリングスカイテクスチャメーカー",
+        },
         "from": "/rolling-sky-spritesheet-maker",
         "to": "/rolling-sky/tools/spritesheet-generator"
     },
     {
+        "name": {
+            "en": "Foo",
+            "en-Shaw": "𐑓𐑵",
+            "ja": "ほげ"
+        },
         "from": "/foo",
         "to": "/bar"
     }
