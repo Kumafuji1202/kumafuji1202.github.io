4e76c2873dfac0a6945f04d9922be77ba9745746
Kumafuji1202 <131581988+Kumafuji1202@users.noreply.github.com>
2025-05-09 17:32:49 +0900

    Automatic redirect system (test)

---
+++ js-global/moved-files.js
@@ -0,0 +1,10 @@
+var movedFiles = [
+    {
+        "from": "/rolling-sky-spritesheet-maker",
+        "to": "/rolling-sky/tools/spritesheet-generator"
+    },
+    {
+        "from": "/foo",
+        "to": "/bar"
+    }
+];
