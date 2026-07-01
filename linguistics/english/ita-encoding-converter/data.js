854ca54c26c75cc10345c1f65f962cdd1583e79c
Kumafuji1202 <131581988+Kumafuji1202@users.noreply.github.com>
2026-07-01 20:11:17 +0900

    ITAEC update again

--- linguistics/english/ita-encoding-converter/data.js
+++ linguistics/english/ita-encoding-converter/data.js
@@ -3,36 +3,42 @@ var systems = {
         name: "PitmanITA",
         font: "PitmanITA",
         supportsCaps: true,
+        supportsU0261: false,
         letters: ['d', 'é', 'í', 'ú', 'œ', 'ž', 'ù', 'ç', 'þ', 'ð', 'š', 'g', 'ñ', 'û', 'a', 'á', 'â', 'ô', 'ó', 'ò', 'õ', 'ý', 'à', 'æ', '’', '‘', '“', '”']
     },
     "fnmk": {
         name: "Phonemics",
         font: "Phonemics",
         supportsCaps: false,
+        supportsU0261: false,
         letters: ['d', 'E', 'I', 'U', 'O', 'Z', 'W', 'C', 'H', 'T', 'S', 'g', 'N', 'R', 'a', '\ufdd0', 'Q', 'F', 'L', 'K', 'G', 'x', 'q', 'A', "'", "'", '"', '"']
     },
     "xim": {
         name: "Xim Sans",
         font: "Xim Sans Handwritten",
         supportsCaps: false,
+        supportsU0261: true,
         letters: ['\uf8f2', '\uf8f3', '\uf8f4', '\uf8fd', 'œ', '\ue05b', '\uf8fe', '\uf8f1', '\uf8fc', '\uf8fb', '\uf8fa', 'g', '\uf8f5', '\uf8f9', 'a', '\uf8ff', '\uf8f0', '\uf8f8', '\uf8f7', '\uf8f6', 'ꞷ', 'ʒ', 'ɑ', 'æ', '’', '‘', '“', '”']
     },
     "nish": {
         name: "Nishiki-teki",
         font: "Nishiki-teki",
         supportsCaps: false,
+        supportsU0261: true,
         letters: ['\ue990', '\ue991', '\ue992', '\ue993', 'œ', '\ue994', '\ue995', '\ue996', '\ue997', '\ue998', '\ue999', 'g', '\ue99a', '\ue99b', '\uee50', '\ue99c', '\ue99d', '\ue99e', '\ue99f', '\ue9a0', 'ꞷ', 'ʒ', 'ɑ', '\uee57', '’', '‘', '“', '”']
     },
     "cat": {
         name: "Catrinity",
         font: "Catrinity",
         supportsCaps: false,
+        supportsU0261: false,
         letters: ['\ue4c0', '\ue4c1', '\ue4c2', '\ue4c3', 'œ', '\ue4c4', '\ue4c5', '\ue4c6', '\ue4c7', '\ue4c8', '\ue4c9', 'ɡ', '\ue4ca', '\ue4cb', 'a', '\ue4cc', '\ue4cd', '\ue4ce', '\ue4cf', '\ue4d0', 'ꞷ', 'ʒ', 'ɑ', 'æ', '’', '‘', '“', '”']
     },
     "uni": {
         name: "Unicode proposal",
         font: "Fairfax HD",
         supportsCaps: false,
+        supportsU0261: true,
         letters: ['d', '\ud837\udf5c', '\ud837\udf5d', 'ᵫ', 'œ', '\ud837\udf66', '\ud837\udf65', '\ud837\udf5b', '\ud837\udf63', '\ud837\udf64', '\ud837\udf62', 'g', 'ŋ', '\ud837\udf61', 'a', '\ud837\udf5a', 'ꜷ', '\ud837\udf60', '\ud837\udf5f', '\ud837\udf5e', 'ꞷ', 'ʒ', 'ɑ', 'æ', '’', '‘', '“', '”']
     },
 };
